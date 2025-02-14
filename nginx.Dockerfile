7e46f0853ba1ee29e516f2bdbfb7695f348cd1f9
Toluskydc <booktolusky@gmail.com>
2025-02-14 07:10:43 -0800

    modified nginx file

--- nginx.Dockerfile
+++
@@ -1,8 +0,0 @@
-# Use official Nginx image
-FROM nginx:latest
-
-# Copy your custom Nginx configuration
-COPY nginx.conf /etc/nginx/nginx.conf
-
-# Expose port 80 for external traffic
-EXPOSE 80
--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e  # Stop script if any command fails
-
-echo "Starting FastAPI..."
-uvicorn main:app --host 0.0.0.0 --port 8000 --proxy-headers &
-
-echo "Starting Nginx..."
-exec nginx -g "daemon off;"
